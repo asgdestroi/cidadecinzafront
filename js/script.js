89ba65542bbbc6162cc3647aa4c2db482e588b9f
asgdestroi <30984139+asgdestroi@users.noreply.github.com>
2025-06-17 19:16:34 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const API_CONFIG = {
     // Para desenvolvimento local
     development: 'http://localhost:5000',
     // Para produção - substitua pela URL do seu backend hospedado
-    production: 'https://asgdestroi.github.io/cidadecinzafront/'
+    production: 'https://cidadecinzabackend.onrender.com'
 };
 
 // Detectar ambiente baseado no hostname
